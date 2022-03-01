8cd8c8db6cdd06672f29834edc4330603d6a19bc
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-03-01 15:39:11 -0500

    fix: emptyline() implemented

--- console.py
+++ console.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
+""" Command interpreter to manage the AirBnB objects.
+    """
 
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    """ _summary_ """
+    """ Contains the entry point of the command interpreter. """
 
     def __init__(self):
-        """ _summary_ """
+        """ Initialize the command interpreter.
+            """
         super().__init__()
         self.prompt = "(hbnb) "
 
-    def do_quit(self, line):
-        """ _summary_ """
-        exit()
+    def emptyline(self):
+        """ Empty line to not execute anything.
+            """
         pass
 
+    def do_quit(self, line):
+        """ Quit command to exit the program.
+            """
+        return True
+
     def do_EOF(self, line):
-        """ _summary_ """
-        exit()
+        """ EOF command to exit the program.
+            """
         return True
 
 
