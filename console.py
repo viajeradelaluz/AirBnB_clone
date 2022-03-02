575396ea23f1228f0bea39c2f43525c99dbcb998
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-03-01 20:13:58 -0500

    feat: create and show methods implemented

--- console.py
+++ console.py
@@ -8,11 +8,89 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     """ Contains the entry point of the command interpreter. """
 
+    classes = ["BaseModel", "User", "State", "City",
+               "Amenity", "Place", "Review"]
+
     def __init__(self):
         """ Initialize the command interpreter.
             """
         super().__init__()
         self.prompt = "(hbnb) "
+
+    def do_create(self, line):
+        """ Creates a new instance of BaseModel.
+            """
+        args = line.split()
+
+        if args[1] == "":
+            print("** class name missing **")
+            return
+
+        if args[1] not in clases:
+            print("** class doesn't exist **")
+            return
+
+        instance = eval(args[1])()
+        instance.save()
+        print(instance.id)
+
+    def do_show(self, line):
+        """ Prints a representation of an instance.
+            """
+        args = line.split()
+
+        if args[1] == "":
+            print("** class name missing **")
+            return
+
+        if args[1] not in clases:
+            print("** class doesn't exist **")
+            return
+
+        if not args[2]:
+            print("** instance id missing **")
+            return
+
+        ##########################
+        # To complete final task #
+        print("** no instance found **")
+        ##########################
+
+    def do_destroy(self, line):
+        """ Deletes an instance.
+            """
+        args = line.split()
+
+        if args[1] == "":
+            print("** class name missing **")
+            return
+
+        if args[1] not in clases:
+            print("** class doesn't exist **")
+            return
+
+        if not args[2]:
+            print("** instance id missing **")
+            return
+
+        ##########################
+        # To complete final task #
+        print("** no instance found **")
+        ##########################
+
+        def do_all(self, line):
+            """ Prints all string representation of all instances.
+                """
+            if args[1] not in clases:
+                print("** class doesn't exist **")
+                return
+
+            #### To complete ###
+
+    def do_update(self, line):
+        """ Updates an instance based on the class name and id.
+            """
+        pass
 
     def emptyline(self):
         """ Empty line to not execute anything.
