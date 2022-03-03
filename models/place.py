c683c39eb2f24af6ce3d94f3157992e52a520c29
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-03-03 02:25:36 +0000

    feat: Place class

---
+++ models/place.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Place class for HBnB
+    """
+
+from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+    """ Place class for HBnB. Inherits from BaseModel
+        """
+    
+    city_id = ""
+    user_id = ""
+    name = ""
+    descriptio = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0.0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
+
+    def __init__(self, *args, **kwargs):
+        """ Init method for place
+            """
+        super().__init__(*args, **kwargs)
